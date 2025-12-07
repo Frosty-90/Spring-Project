e45f5b43df723fdc19dc468737037c599df1a514
Spring-Project <idcwhatyoulike@gmail.com>
2025-12-07 22:12:08 +0530

    Revert "final changes"
    This reverts commit f0d3197a5cd642c8c1610fcebb6bfc609485f0e6.

--- rideshare/src/main/java/com/mshreesha/rideshare/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mshreesha.rideshare.exception;
-
-public class NotFoundException extends RuntimeException {
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
